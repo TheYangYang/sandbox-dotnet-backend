ccf2853dba34c4b46d010e93589eedb8006aaed0
Chang Geng <changgeng333@gmail.com>
2025-12-05 14:01:37 -0600

    test: add failed sample tests

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Sandbox.Tests
+{
+    public class FailingTests
+    {
+        [Fact]
+        public void ThisTestShouldFail()
+        {
+            // This will always fail
+            Assert.Equal(1, 2);
+        }
+    }
+}
