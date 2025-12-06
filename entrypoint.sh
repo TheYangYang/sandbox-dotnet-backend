ba8c06517a01275a246e908f33dde7f09e540163
Chang Geng <changgeng333@gmail.com>
2025-12-05 18:50:03 -0600

    fix: fix launching setting in dockerfile prod

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+# Replace $PORT placeholder in nginx config
+sed -i "s/{{PORT}}/${PORT}/g" /etc/nginx/conf.d/default.conf
 
+# Start Nginx in background
 nginx -g 'daemon off;' &
 
+# Start .NET app on Render's PORT
 echo "Starting .NET application on port ${PORT}..."
-exec dotnet Api.dll
+exec dotnet Api.dll --urls "http://0.0.0.0:${PORT}"
