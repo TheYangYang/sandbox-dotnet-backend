a45d0ba809de4aff1429517c27ea24d187fc7cc8
Chang Geng <changgeng333@gmail.com>
2025-12-05 19:54:23 -0600

    chore: remove nginx from dockerfile prod

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-# Replace $PORT placeholder in nginx config
-sed -i "s/{{PORT}}/${PORT}/g" /etc/nginx/conf.d/default.conf
-
-# Start Nginx in background
-nginx -g 'daemon off;' &
 
 # Start .NET app on Render's PORT
 echo "Starting .NET application on port ${PORT}..."
